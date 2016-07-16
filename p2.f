f089f25acf8f85fc62e67659b0517fae64154395
Andrew Smiley <andrew.smiley@live.com>
2016-07-16 12:26:37 -0400

    test

--- p2.f
+++ p2.f
@@ -1,34 +1,27 @@
-      PROGRAM helloWorld
+        PROGRAM helloWorld
 	      REAL transactions(100, 4)
-        CHARACTER  name*20, address*20, city*15, province*15, balance*15
+        CHARACTER  name*15, address*25, city*15, province*10, balance*7
         CHARACTER invoicefilename*20
         CHARACTER output
         REAL customer
-        CHARACTER ccustomer*5, tcustomer*5,titem*6,tquantity*1, tcode*1
+        CHARACTER ccustomer*5, tcustomer*5,titem*6,tquantity*1
+        CHARACTER tcode*1
         INTEGER tlines, reason,clines
-        INTEGER ios
+        INTEGER cios, tios
         CHARACTER t4
+        CHARACTER error*50
         tlines=0
 *open the transactions file
-	      OPEN(unit=2, file="customers.dat")
+	      OPEN(unit=3, file="customers.dat")
         DO
-          READ(2,*, IOSTAT=ios) ccustomer, name, address, city, province
+          READ(3,*, IOSTAT=cios) ccustomer, name, address, city, province, balance
           print *, name
-          invoicefilename = "invoices/"//ccustomer//".dat"
-          OPEN(unit=1, FILE=invoicefilename,STATUS='OLD')
-          output = name//' '//address//' '//city//' '//province
-*          print *, output
-          WRITE(1, '(A)') name//address//city//province
-*          WRITE(1, '(/)') address
-          CLOSE(1)
-          IF (ios > 0) THEN
-            EXIT
-          ELSE IF (ios < 0) THEN
-            PRINT *, 'EOF REACHED'
-            EXIT
-          ELSE
 
-          END IF
+          invoicefilename = "invoices/"//ccustomer//".dat"
+          OPEN(unit=1, FILE=invoicefilename,STATUS='NEW')
+          WRITE(1, '(A)') name//address//city//province
+          CLOSE(1)
+
           tlines = tlines+1
         END DO
 *iterate over it and add all the values to the array
